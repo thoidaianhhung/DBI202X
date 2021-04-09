4424efc0951845e5aba0fefdf98e30bdf66fdfeb
thoidaianhhung <62500655+thoidaianhhung@users.noreply.github.com>
2021-04-09 17:29:23 +0700

    Lab4

---
+++ DBI202x_AS2_NGUYENVANHUNG/INSERT_BIENTAPVIEN.sql
@@ -0,0 +1,11 @@
+﻿INSERT INTO BIENTAPVIEN
+VALUES ('PT01', 'Bùi Ngọc Anh', 'Hải Dương', '0973695828', '2000-07-24', 'anhbn@funix.edu.vn', 'Biên Tập Viên'),
+('PT02', 'Nguyễn Lan Anh', 'Hải Phòng', '0973009009', '2000-02-14', 'anhln@funix.edu.vn', 'Biên Tập Viên'),
+('PT03', 'Nguyễn Thùy Anh', 'Hà Nội', '0973300300', '2000-09-20', 'anhnt@funix.edu.vn', 'Biên Tập Viên'),
+('PT04', 'Nguyễn Trọng Bảo', 'Vĩnh Phúc', '0973768989', '2000-08-10', 'baont@funix.edu.vn', 'Biên Tập Viên'),
+('PT05', 'Hà Minh Giang', 'Hải Dương', '0973695829', '2000-07-24', 'gianghm@funix.edu.vn', 'Biên Tập Viên'),
+('PT06', 'Lê Hoàng Hà', 'Hải Dương', '0973695528', '2000-07-24', 'halh@funix.edu.vn', 'Biên Tập Viên'),
+('PT07', 'Nguyễn Thị Khánh Huyền', 'Hải Dương', '0973655828', '2000-07-24', 'huyenntk@funix.edu.vn', 'Biên Tập Viên'),
+('PT08', 'Thái Thị Lan', 'Hải Dương', '0973695828', '2000-07-24', 'anhbn@funix.edu.vn', 'Biên Tập Viên'),
+('PT09', 'Dương Tuyết Nhi', 'Hải Dương', '0973695828', '2000-07-24', 'anhbn@funix.edu.vn', 'Biên Tập Viên'),
+('PT10', 'Đặng Thị Trâm', 'Hải Dương', '0973695828', '2000-07-24', 'anhbn@funix.edu.vn', 'Biên Tập Viên');
