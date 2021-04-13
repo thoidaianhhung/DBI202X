a68bbecb33dc4d3f3d6bc9b417d583f5e115f20e
thoidaianhhung <62500655+thoidaianhhung@users.noreply.github.com>
2021-04-13 17:09:40 +0700

    Thực hành

--- DBI202x_AS2_NGUYENVANHUNG/TRIGGER.sql
+++ DBI202x_AS2_NGUYENVANHUNG/TRIGGER.sql
@@ -1,8 +1,10 @@
 ﻿--Câu lệnh tạo Trigger đảm bảo ràng buộc khi thêm 1 bài báo vào CSDL, cần đảm bảo
---phóng viên viết bài báo này đã có trong bảng PHONGVIEN. Trường hợp phóng viên không
---có trong bảng PHONGVIEN thì insert into PHONGVIEN. Trong đó MAPV, TENPV giống nhau,
---MAPT được lấy từ BAIBAO, các thông tin còn lại NULL.
-CREATE TRIGGER BAIBAO_PHONGVIEN ON BAIBAO AFTER INSERT AS
+--tên bài báo không trùng lặp với tên bài báo đã có trước đó. Nếu trùng lặp thì
+--thực hiện thông báo sau đó xóa bài báo.
+CREATE TRIGGER CheckPhongVien ON BAIBAO AFTER INSERT AS
 BEGIN
-	
+	DECLARE @TENBB NVARCHAR(30);
+	SELECT A.MABB AS BaiBaoA, B.MABB AS BaiBaoB, A.TENBB
+	FROM BAIBAO A, BAIBAO B
+	WHERE A.TENBB = B.TENBB AND A.MABB <> B.MABB;
 END
